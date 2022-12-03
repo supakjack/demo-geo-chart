71dd92a92c7bb379673f543aeea88cc03660ee1a
supakjack <supakjack@gmail.com>
2022-12-03 14:19:05 -0800

    add  leaflet to demo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+onMounted(() => {
+    const map = L.map('map').setView([13.32916, 459.865551], 13)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map)
+
+    const marker = L.marker([13.32916, 459.865551]).addTo(map)
+})
+</script>
 <template>
-   <h1>Hello World</h1>
+    <div id="map" class="h-screen"></div>
 </template>
